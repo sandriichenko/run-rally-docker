b038b3dddec5c03fc35f91946336c224e24fca0e
sandriichenko <sandriichenko@mirantis.com>
2017-03-14 11:58:47 +0200

    Add base functionality

---
+++ run_docker.sh
@@ -0,0 +1,9 @@
+apt-get install -y docker.io
+apt-get install -y cgroup-bin
+docker pull rallyforge/rally:0.8.1
+cp /root/openrc /home
+image_id=$(docker images | grep 0.8.1| awk '{print $3}')
+docker run --net host -v /home/:/home/rally -tid -u root $image_id
+docker_id=$(docker ps | grep $image_id | awk '{print $1}'| head -1)
+docker exec -ti $docker_id bash
+docker exec -ti $docker_id bash -c "./run_tempest_with_ironic.sh"
---
+++ run_tempest_with_ironic.sh
@@ -0,0 +1,13 @@
+cd /var/lib/
+git clone https://git.openstack.org/openstack/tempest -b 14.0.0 &&     pip install tempest==14.0.0
+source /home/rally/openrc 
+rally-manage db recreate
+rally deployment create --fromenv --name=tempest
+rally verify create-verifier --type tempest --name tempest-verifier --source /var/lib/tempest --version 14.0.0 --system-wide
+wget https://raw.githubusercontent.com/obutenko/rally_tempest_docker/master/tempest_conf/mos10_lvm.conf
+rally verify configure-verifier --extend mos10_lvm.conf 
+rally verify configure-verifier --show
+wget https://raw.githubusercontent.com/openstack/ironic/master/test-requirements.txt
+pip install -r test-requirements.txt
+rally verify add-verifier-ext --source https://github.com/openstack/ironic.git
+rally verify list-verifier-tests
