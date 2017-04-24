34b20dbd9e42f2736ed4f9a4ab8929de28b2416c
sandriichenko <sandriichenko@mirantis.com>
2017-04-24 14:37:52 +0000

    Fix for plugins

--- install_plugin.sh
+++ install_plugin.sh
@@ -2,7 +2,7 @@ if [ ${1} == ironic ]
 then
     git clone https://github.com/openstack/ironic.git
     pip install -r ironic/test-requirements.txt
-    rally verify add-verifier-ext --source ironic
+    rally verify add-verifier-ext --source /home/rally/ironic
 fi
 
 if [ ${1} == designate ]
@@ -10,14 +10,14 @@ then
     pip install ddt==1.0.1
     git clone https://github.com/openstack/designate-tempest-plugin.git
     pip install -r designate-tempest-plugin/test-requirements.txt
-    rally verify add-verifier-ext --source designate-tempest-plugin
+    rally verify add-verifier-ext --source /home/rally/designate-tempest-plugin
 fi
 
 if [ ${1} == ceilometer ]
 then
     git clone https://github.com/openstack/ceilometer.git
     pip install -r ceilometer/test-requirements.txt
-    rally verify add-verifier-ext --source ceilometer
+    rally verify add-verifier-ext --source /home/rally/ceilometer
 fi
 
 rally verify configure-verifier --extend temp.conf --reconfigure --show
